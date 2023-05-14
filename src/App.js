0f92b80461752ef310c1102a3f986f9bf255d0b5
ASEN <as.weeramuni@ncs.com.sg>
2023-05-14 16:23:43 +0800

    feat(): material UI theme setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import TopBar from "./scenes/global/TopBar";
+import { useMode, ColorModeContext } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className="content">
+            <TopBar/>
+          </main>
+          App
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
